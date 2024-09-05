a036cc391ee22a91e99f4cee2167c69c0e7ec5a4
Shigatawa <keigo-tamura0420@keio.jp>
2024-09-05 11:09:18 +0900

    update test

--- test.bash
+++ test.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2024 Keigo Tamura
+#SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at line $1
+	res=1
+}
+
+res=0
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
 
